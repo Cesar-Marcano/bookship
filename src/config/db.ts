18a7216dd0362ae2b961915d4d4315dc51a2330d
Cesar-Marcano <marcanocesar86@gmail.com>
2025-02-21 15:58:56 -0400

    feat: add PostgreSQL database configuration and connection pooling

---
+++ src/config/db.ts
@@ -0,0 +1,31 @@
+import { Pool } from "pg";
+import dotenv from "dotenv";
+import logger from "./logger";
+
+dotenv.config();
+
+class Database {
+  private static instance: Pool;
+
+  private constructor() {}
+
+  public static getInstance(): Pool {
+    if (!Database.instance) {
+      Database.instance = new Pool({
+        connectionString: process.env["DATABASE_URL"],
+        max: 10,
+        idleTimeoutMillis: 30000,
+      });
+
+      Database.instance.on("error", (err) => {
+        logger.error("PostgreSQL Pool Error:", err);
+      });
+
+      logger.info("Database (PostgreSQL) connected.")
+    }
+
+    return Database.instance;
+  }
+}
+
+export const db = Database.getInstance();
