d7db8d53dab8c97accb0020174a73421cb649ab0
Cesar-Marcano <marcanocesar86@gmail.com>
2025-02-22 13:11:12 -0400

    refactor: remove unused sum utility and related tests

--- src/utils/index.ts
+++
@@ -1,2 +0,0 @@
-export const sum = (...numbers: number[]): number =>
-  numbers.reduce((a: number, b: number) => a + b, 0);
--- tests/example.spec.ts
+++
@@ -1,7 +0,0 @@
-import { sum } from "../src/utils";
-
-describe("Sample Utility Functions", () => {
-  it("should sum two numbers correctly", () => {
-    expect(sum(1, 2)).toBe(3);
-  });
-});
