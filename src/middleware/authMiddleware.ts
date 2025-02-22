a4de5e8a8728b846f393c54bd3814a88daf2d4bc
Cesar-Marcano <marcanocesar86@gmail.com>
2025-02-22 17:29:50 -0400

    feat: add role-based access control middleware

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,7 +1,22 @@
 import { Request, Response, NextFunction } from "express";
 import passport from "passport";
+import { Role } from "../types/role";
+import { getUser } from "../utils/getUser";
 
 export const isAuthenticated = passport.authenticate("jwt", { session: false });
+
+export const hasRole =
+  (roles: Role[]) =>
+  (req: Request, res: Response, next: NextFunction): void => {
+    const user = getUser(req);
+
+    if (!roles.includes(user.userData.role!)) {
+      res.status(403).json({ message: "Forbidden" });
+      return;
+    }
+
+    next();
+  };
 
 export const isAuthorized = (
   req: Request,
