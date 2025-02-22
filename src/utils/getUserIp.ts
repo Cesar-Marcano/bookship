ae3679a3ffc97feef7e982954c1376c4b709db9c
Cesar-Marcano <marcanocesar86@gmail.com>
2025-02-22 16:45:50 -0400

    refactor: simplify getUserIp function

--- src/utils/getUserIp.ts
+++ src/utils/getUserIp.ts
@@ -1,10 +1,5 @@
 import { Request } from "express";
-import { usesLoadBalancer } from "../config";
 
 export const getUserIp = (req: Request): string => {
-  if (!usesLoadBalancer) {
-    return req.socket.remoteAddress || "Unknown ip.";
-  } else {
-    return req.ip || "Unknown ip.";
-  }
+  return req.ip || "Unknown ip.";
 };
