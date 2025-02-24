df421c2afdf8562cf86bc37534029e19369dc217
Cesar-Marcano <marcanocesar86@gmail.com>
2025-02-24 19:44:24 -0400

    feat: configure cookie-parser middleware and update CORS settings

--- src/app.ts
+++ src/app.ts
@@ -14,23 +14,29 @@ import { errorFilter } from "./middleware/filter";
 import { isProduction, usesLoadBalancer } from "./config";
 import "./config/passport";
 import passport from "passport";
+import cookieParser from "cookie-parser";
 
 // Express App Instance
 export const app = express();
 
 // Trust the proxy in case of load balancer
-if (isProduction)
-  app.set("trust proxy", usesLoadBalancer);
+if (isProduction) app.set("trust proxy", usesLoadBalancer);
 
 // =====================
 //       MIDDLEWARES
 // =====================
 
 // Parses incoming requests with JSON payloads
+app.use(cookieParser());
 app.use(json({ limit: "1mb" }));
 
 // Security
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 app.use(helmet());
 app.use(hpp());
 app.use("/api", rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }));
