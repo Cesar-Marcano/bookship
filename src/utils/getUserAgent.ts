fdbd3fa91a3b3e1e4c452c0e9e2668ae0b864e42
Cesar-Marcano <marcanocesar86@gmail.com>
2025-02-24 19:47:16 -0400

    feat: normalize user agent using UAParser in getUserAgent

--- src/utils/getUserAgent.ts
+++ src/utils/getUserAgent.ts
@@ -1,5 +1,20 @@
 import { Request } from "express";
+import { UAParser } from "ua-parser-js";
+import { isProduction } from "../config";
 
 export const getUserAgent = (req: Request): string => {
-  return req.headers["user-agent"] || "Unknown user agent.";
+  const userAgent = req.headers["user-agent"] || "Unknown user agent.";
+
+  if (userAgent.startsWith("Unknown")) {
+    return userAgent;
+  } else if (!isProduction && userAgent.startsWith("Postman")) {
+    return userAgent;
+  }
+
+  const parser = new UAParser();
+  const result = parser.setUA(userAgent).getResult();
+
+  const normalizedUserAgent = `${result.browser.name} ${result.browser.major} (${result.os.name} ${result.os.version})`;
+
+  return normalizedUserAgent;
 };
